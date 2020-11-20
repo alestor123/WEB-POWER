ec32f35e260840fe0d326a39bee00bebbdbb35e7
alestor123 <alestoraldous@gmail.com>
2020-11-20 23:18:00 +0530

    added cli

--- App.js
+++ App.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
 require('dotenv').config()
+var express = require('express'),
+app = express(),
+pck = require('./package.json'),  
+port = process.env.PORT || argv || 3000;
+if(argv== '-v' ||argv == '--version'){
+    console.log( `${pck.version}`)
+  process.exit(1);
+}
+else if (argv =='-h'|| argv == '--help') { // checking undifined args
+    console.log(`
+    Usage: badge-generator <Port> 
+`);
+}
+else{
+    app.listen(port, () => console.log(`server running at ${port}`))
+}
